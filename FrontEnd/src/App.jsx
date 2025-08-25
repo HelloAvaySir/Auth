e0e9bb366f13adc68449acc45fc7e0115ae3d9c7
HelloAvaySir <adhikariavay123@gmail.com>
2025-08-25 17:27:53 +0545

    Changed the name of the index.css

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Fix the import paths based on your actual file structure
 import Navbar from "./component/Global/Navbar.jsx";  // Add .jsx extension
 import Home from "./pages/Home.jsx";
-import Login from "./component/form/login.jsx";     // Check if it's lowercase 'login'
+import Login from "./component/form/login.jsx";     
 import Register from "./component/form/Register.jsx";
 import About from "./pages/About.jsx";
 import ContactUs from "./pages/ContactUs.jsx";
